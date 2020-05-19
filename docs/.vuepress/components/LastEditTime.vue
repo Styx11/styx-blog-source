64a5b6aa940bf10ffaceffc875e5f214a8e45d58
styx11 <1654630248@qq.com>
2020-05-19 10:25:12 +0800

    :sparkles: update LastEditTime component for requesting github api

--- docs/.vuepress/components/LastEditTime.vue
+++ docs/.vuepress/components/LastEditTime.vue
@@ -22,6 +22,50 @@ export default {
     lastEditTime: '',
     baseCommitApi: 'https://api.github.com/repos/styx11/styx-blog-source/commits?path=docs/blog/',
   }),
+  computed: {
+    completeCommitApi () {
+      let path = this.filepath;
+      if (path.indexOf('/') === 0) path = path.substring(1);
+      return this.baseCommitApi + path;
+    }
+  },
+  mounted () {
+    // 因为使用场景有限，所以忽略了一些错误处理
+    axios.get(this.completeCommitApi)
+      .then(res => {
+        if (!res || res.status !== 200) return;
+        const { data } = res;
+        const lastCommit = (!!data[0].commit) && data[0].commit;
+        const lastCommitter = (!!lastCommit.committer) && lastCommit.committer;
+        const lastCommitDate = (!!lastCommit.committer.date) && lastCommit.committer.date;
+        if (!lastCommitDate) return;
+
+        // convert date i.e 2020-05-17T08:31:38Z into 2020-05-17, 16:31:38 PM
+        let lastEditTime = '';
+        const lastDate = new Date(lastCommitDate);
+        const year = lastDate.getFullYear();
+        const day = this.formatTime(lastDate.getDate());
+        const month = this.formatTime((lastDate.getMonth() + 1) % 12);
+        lastEditTime += `${year}-${month}-${day}, `;
+
+        const hour = this.formatTime(lastDate.getHours());
+        const second = this.formatTime(lastDate.getSeconds());
+        const minutes = this.formatTime(lastDate.getMinutes());
+        lastEditTime += `${hour}:${minutes}:${second} ${hour < 12 ? 'AM' : 'PM'}`;
+
+        this.lastEditTime = lastEditTime;
+        this.ready = true;
+      }).catch(e => {
+        console.error(e);
+        this.ready = false;
+      });
+  },
+  methods: {
+    // format date(second. hour...) into XX
+    formatTime (date) {
+      return date < 10 ? `0${date}` : date;
+    }
+  }
 }
 </script>
 
